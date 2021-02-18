4b7bf42388c5e4bdb08f6bd78a32c387488f49a6
Jan Soendermann <jan.soendermann+git@gmail.com>
2021-02-18 20:04:27 +0000

    Client

--- client/countdown.py
+++ client/countdown.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-import requests
-# import fourletterphat as flp
+from operator import itemgetter
+from datetime import datetime
+from requests import get
+import fourletterphat as flp
+from time import sleep
 
-res = requests.get('https://countdown.j51.eu/countdown')
-days = r.text
+def flp_print(msg):
+  flp.print_str(str(msg))
+  flp.show()
 
-flp.print_str(str(days))
-flp.show()
+while True:
+  m, d = itemgetter('month', 'day')(datetime.today())
+  if m == 2 and d == 18:
+    flp_print("JAN")
+  else:
+    res = get('https://countdown.j51.eu/countdown')
+    days = res.text
+    flp_print(days)
+
+  sleep(60)
