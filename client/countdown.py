6112ec30153ea580d27ec72ea3f065567b987d9f
Jan Soendermann <jan.soendermann+git@gmail.com>
2021-04-05 20:15:40 +0100

    Logging

--- client/countdown.py
+++ client/countdown.py
@@ -16,16 +16,21 @@ while True:
     day = today.day
 
     if month == 4 and day == 3:
+      print("Emma day!")
       flp_print("EMMA")
     elif month == 11 and day == 2:
+      print("Jan day!")
       flp_print("JAN")
     elif hour < 8 or hour >= 23:
+      print("Before 8 or after 23: {}".format(hour))
       flp.clear()
     else:
+      print("Fetching updated countdown...")
       res = get('https://countdown.j51.eu/countdown')
       days = res.text
       flp_print(days)
   except:
+    print("Error")
     pass
 
   sleep(60)
